4868c22800d7df5e3b0b8eeea82f54099d59d71f
Devin Gillogly <virlym@gmail.com>
2020-10-06 20:28:02 -0700

    added comments

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+// local file includes
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+const render = require("./lib/htmlRenderer.js");
+const questionBuilder = require("./lib/questionBuilder.js");
+
+// outside includes
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// global variables
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
-const questionBuilder = require("./lib/questionBuilder.js");
-
 const employees = [];
 let htmlDoc;
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// get the role of the new employee
 function addEmployee(){
     inquirer.prompt(questionBuilder.questionBuilder("Init")).then(function (response) {
         if (response.roleAns === "Intern") {
@@ -32,6 +32,8 @@ function addEmployee(){
     });
 }
 
+// add an employee to the list of the Intern role
+// then either add a new employee or create the html document
 function addIntern(){
     inquirer.prompt(questionBuilder.questionBuilder("Intern")).then(function (response) {
         employees.push(new Intern(response.nameAns, response.idAns, response.emailAns, response.schoolAns));
@@ -45,6 +47,8 @@ function addIntern(){
     });
 }
 
+// add an employee to the list of the Engineer role
+// then either add a new employee or create the html document
 function addEngineer(){
     inquirer.prompt(questionBuilder.questionBuilder("Engineer")).then(function (response) {
         employees.push(new Engineer(response.nameAns, response.idAns, response.emailAns, response.githubAns));
@@ -58,6 +62,8 @@ function addEngineer(){
     });
 }
 
+// add an employee to the list of the Manager role
+// then either add a new employee or create the html document
 function addManager(){
     inquirer.prompt(questionBuilder.questionBuilder("Manager")).then(function (response) {
         employees.push(new Manager(response.nameAns, response.idAns, response.emailAns, response.officeAns));
@@ -70,15 +76,9 @@ function addManager(){
         }
     });
 }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// if the create the html file in the output folder
+// if the output folder does not exist, create it
 function makeFile(){
     if (!fs.existsSync("./output")){
         console.log("make dir if");
@@ -104,13 +104,5 @@ function makeFile(){
     }
 }
 
+// start the cycle
 addEmployee();
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
